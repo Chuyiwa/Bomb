4a8d622257db7e1eb1d7b34e045922c5d44291ce
CHu Wang <38710654+Chuyiwa@users.noreply.github.com>
2018-11-07 11:08:15 +0800

    Add files via upload

---
+++ Bomb.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+
+
+    public GameObject explosionPrefab;
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Instantiate(explosionPrefab,
+                        this.transform.position,
+                        this.transform.rotation);
+            
+        }
+        
+    }
+}
